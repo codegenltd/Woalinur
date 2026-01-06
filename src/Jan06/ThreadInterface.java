276eb28dd48a0cc7b07dc0cb9c78727c5d3c8032
md woalinur <mdwoalinur@gmail.com>
2026-01-06 16:35:09 +0600

    thread

---
+++ src/Jan06/ThreadInterface.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jan06;
+
+import practice_05.*;
+
+/**
+ *
+ * @author HP
+ */
+public class ThreadInterface implements Runnable {
+
+    @Override
+    public void run() {
+       for( int i = 0; i<5; i++){
+           System.out.println("Runnable interface in implements by thread");
+       }
+    }
+    
+}
--- src/Jan06/ThreadTestMainClass.java
+++ src/Jan06/ThreadTestMainClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practice_05;
+package jan06;
 
 /**
  *
