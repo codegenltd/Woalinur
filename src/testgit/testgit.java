f89f47db29d50a99799f9bef180ebf4661e4b30f
md woalinur <mdwoalinur@gmail.com>
2026-01-25 11:28:27 +0600

    test git

---
+++ src/testgit/testgit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author HP
+ */
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("test git");
+        System.out.println("git test");
+    }
+}
